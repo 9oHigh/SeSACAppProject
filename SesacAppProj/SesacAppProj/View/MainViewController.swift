d58651eb6252b5543f7d0ffdf2f50bbc4bb23eb7
9oHigh <wkdska957@naver.com>
2022-01-04 00:33:20 +0900

    Project Init

---
+++ SesacAppProj/SesacAppProj/View/MainViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  SesacAppProj
+//
+//  Created by 이경후 on 2022/01/04.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+}
+
